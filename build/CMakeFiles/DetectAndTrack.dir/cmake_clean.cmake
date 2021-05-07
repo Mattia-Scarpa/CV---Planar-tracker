d8b502ab14d4a77b73d228408dffdf408c69d99c
Mattia <scarpa.mat25@gmail.com>
2021-05-07 20:08:35 +0200

    first load

---
+++ build/CMakeFiles/DetectAndTrack.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DetectAndTrack.dir/src/main.cpp.o"
+  "DetectAndTrack"
+  "DetectAndTrack.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DetectAndTrack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+#include <opencv2/core.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/xfeatures2d.hpp>
+#include <opencv2/core.hpp>
+
+using namespace cv;
+
+int main(int argc, char const *argv[]) {
+
+  // Defining Image path
+  vector<string> dataPaths {"../data/objects/*.jpg", "../data/objects/*.png"};
+  vector<string> imagesPath;
+  vector<string> imagesPath_temp;
+  for (size_t  i(0); i < dataPaths.size(); ++i) {
+    glob(dataPaths[i], imagesPath_temp);
+    for (size_t j(0); j < imagesPath_temp.size(); ++j) {
+            imagesPath.push_back(imagesPath_temp[j]);
+    }
+  }
+
+  if (imagesPath.size() < 1) {
+    cout << "No compatible images found in folder ../data/ \nAdd image in the specified folder then rerun the application (available format: .jpg, .png)" << endl;
+    return 0;
+  }
+
+  cout << "A total of " << imagesPath.size() << "images objects to detect has been found!" << endl;
+
+  /*for (size_t i(0); i < imagesPath.size(); i++) {
+    cout << i << ": " << imagesPath[i].substr(7) << endl;
+  }*/
+
+  dataPaths = {"../data/*.avi", "../data/*.mov", "../data/*.mp4"}
+
+  cv::VideoCapture cap("video.avi");
+  bool detected = false; // chek if objects has been detected from the first frame
+
+  if(cap.isOpened()) { // check if we succeeded
+    for(;;) {
+      Mat frame;
+      cap >> frame;
+
+      cout << "I am doing something" << endl;
+    }
+  }
+  else {
+    cout <<
+  }
+
+  return 0;
+}

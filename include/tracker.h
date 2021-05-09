b952e405fb86914e9315fd3bd933409643cd9f87
Mattia <scarpa.mat25@gmail.com>
2021-05-09 19:54:47 +0200

    separate class option

---
+++ include/tracker.h
@@ -0,0 +1,15 @@
+#include <opencv2/core.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/xfeatures2d.hpp>
+
+
+class tracker{
+
+public:
+
+  tracker(cv::Mat& src);
+
+private:
+}
